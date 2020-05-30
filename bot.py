c3761c1e5e986b56a04545b9b30fe81e36ae6bdf
Varun Jindal <vjindal0112@gmail.com>
2020-05-30 00:27:03 -0400

    get rid of webhook

--- bot.py
+++ bot.py
@@ -41,24 +41,24 @@ def main():
     # Post version 12 this will no longer be necessary
     updater = Updater(TOKEN, use_context=True)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://standup-telegram-bot.herokuapp.com/' + TOKEN)
+#    # Get the dispatcher to register handlers
+#    dp = updater.dispatcher
+#
+#    # on different commands - answer in Telegram
+#    dp.add_handler(CommandHandler("start", start))
+#    dp.add_handler(CommandHandler("help", help))
+#
+#    # on noncommand i.e message - echo the message on Telegram
+#    dp.add_handler(MessageHandler(Filters.text, echo))
+#
+#    # log all errors
+#    dp.add_error_handler(error)
+#
+#    # Start the Bot
+#    updater.start_webhook(listen="0.0.0.0",
+#                          port=int(PORT),
+#                          url_path=TOKEN)
+#    updater.bot.setWebhook('https://standup-telegram-bot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
